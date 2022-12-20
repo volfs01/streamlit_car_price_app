4c43b79e1f9bf46c9c404a1fcdd358619494fbb0
volfs01 <120348531+volfs01@users.noreply.github.com>
2022-12-20 12:02:45 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def run_home_app() :
     
-    st.text(' !!@!!!!!!@@@@@@@@@@@!')
+    st.text(' ')
     
     st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다. 데이터분석 및 고객정보를 넣으면 얼마정도의 차를 구매할지 예측합니다.')
     
